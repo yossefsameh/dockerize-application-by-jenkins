bba8ff3fcdff81367fa0227dd514f93552e96219
yossefsameh <yossefsameh23@gmail.com>
2022-11-16 18:03:36 +0200

    edit

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "Hhhhhello Updated World!"
+    return "Hhhhello Updated World!"
 
 # if __name__ == '__main__':
 #     app.run()
